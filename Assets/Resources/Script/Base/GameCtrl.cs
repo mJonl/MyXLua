b3a1ad5e1c403fee58331b987e6bd609281313e0
Json <mjonson@sina.com>
2019-10-26 20:38:26 +0800

    新建项目

---
+++ Assets/Resources/Script/Base/GameCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyExamp
+{
+    public class GameCtrl : MonoBehaviour
+    {
+        internal static XLua.LuaEnv luaEnv = new XLua.LuaEnv();//全局唯一的
+        void Start()
+        {
+            // 恒定帧率
+            Application.targetFrameRate = 60;
+            //Screen.SetResolution(1920, 1080, true);//这是设置屏幕分辨率的方法，后面的false表示非全屏,测试用800*600
+            Screen.SetResolution(800, 600, true);//这是设置屏幕分辨率的方法，后面的false表示非全屏,测试用800*600
+            InitBattle();
+            InitHomeView();
+        }
+
+        void InitBattle()
+        {
+            GameObject.Find("BattleCtrl").AddComponent<MyLuaBehaviour>().InitLuaScript();
+            
+        }
+        void InitHomeView()
+        {
+            GameObject.Find("Canvas").AddComponent<MyLuaBehaviour>().InitLuaScript();
+        }
+    }
+}
+
---
+++ Assets/Resources/Script/Base/MyLuaBehaviour.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System;
+namespace MyExamp
+{
+    [System.Serializable]//1.方便存硬盘2.方便传输3.类的所有属性编辑界面显示
+    public class Injection
+    {
+        public string name;
+        public GameObject value;
+    }
+    //打了标签，lua就可以访问类的所有属性及方法
+    [LuaCallCSharp]//教程说如果不打，就用性能较低的方式访问
+    public class MyLuaBehaviour : MonoBehaviour
+    {
+        public TextAsset luaScript;
+        public Injection[] injections;
+
+       // internal static LuaEnv luaEnv = new LuaEnv();
+        internal static float lastGCTime = 0;
+        internal static float GCInterval = 1;//1 scond
+
+        private Action luaStart;
+        private Action luaUpdate;
+        private Action luaOnDestroy;
+
+        private LuaTable scriptEnv;
+
+        private void Awake()
+        {
+            scriptEnv =  GameCtrl.luaEnv.NewTable();
+            //为每个脚本设立一个独立的环境，防止污染变量
+            LuaTable meta = GameCtrl.luaEnv.NewTable();
+            meta.Set("__index", GameCtrl.luaEnv.Global);
+            scriptEnv.SetMetaTable(meta);
+            meta.Dispose();
+
+            scriptEnv.Set("self", this);
+            if(injections != null)
+            {
+                foreach (var injection in injections)//设置lua变量值
+                {
+                    scriptEnv.Set(injection.name, injection.value);
+                }
+            }
+           
+           
+            if(luaScript != null)
+            {
+                LoardLuaScript(luaScript.text);
+            }
+        }
+
+        public void InitLuaScript()
+        {
+
+            string scriptName = "";
+            if (Protocol.ScriptName.ContainsKey(gameObject.name))  
+            {
+                scriptName = Protocol.ScriptName[gameObject.name];
+            }
+            else
+            {
+                scriptName = string.Format("Script/UI/{0}.lua", gameObject.name);
+            }
+            luaScript = Resources.Load<TextAsset>(scriptName);
+            LoardLuaScript(luaScript.text);
+        }
+        public void LoardLuaScript(string str)
+        {
+            GameCtrl.luaEnv.DoString(str, "LuaTestScript", scriptEnv);
+           
+            Action luaAwake = scriptEnv.Get<Action>("awake");
+            scriptEnv.Get("start", out luaStart);
+            scriptEnv.Get("update", out luaUpdate);
+            scriptEnv.Get("ondestroy", out luaOnDestroy);
+
+            if (luaAwake != null)
+            {
+                luaAwake();
+            }
+        }
+
+        private void Start()
+        {
+            if(luaStart != null)
+            {
+                luaStart();
+            }
+        }
+
+        private void Update()
+        {
+            if(luaUpdate != null)
+            {
+                luaUpdate();
+            }
+            if(Time.time - MyLuaBehaviour.lastGCTime > GCInterval)
+            {
+                GameCtrl.luaEnv.Tick();
+                MyLuaBehaviour.lastGCTime = Time.time;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(luaOnDestroy != null)
+            {
+                luaOnDestroy();
+            }
+            luaOnDestroy = null;
+            luaUpdate = null;
+            luaStart = null;
+            scriptEnv.Dispose();
+            injections = null;
+        }
+    }
+}
+
